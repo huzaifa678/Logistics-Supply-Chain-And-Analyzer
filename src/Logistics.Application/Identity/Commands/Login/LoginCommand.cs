472e4d24969047dfaf3ed186c45c8b184869e426
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-06-17 02:18:30 +0500

    feat(application): Login command and validator

---
+++ src/Logistics.Application/Identity/Commands/Login/LoginCommand.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using Logistics.Application.Common.Models;
+using MediatR;
+
+namespace Logistics.Application.Identity.Commands.Login;
+
+public sealed record LoginCommand(string Email, string Password) : IRequest<Result<AuthResult>>;
+
+public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty();
+    }
+}
